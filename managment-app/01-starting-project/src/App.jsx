cd5e75d7e0d69363d4ef9b7a5735c73ef3172779
Denys <dh92fr@gmail.com>
2026-02-02 17:49:36 +0200

    create app managment-app , add aside bar

---
+++ managment-app/01-starting-project/src/App.jsx
@@ -0,0 +1,11 @@
+import PanelProjects from "./components/Panel/aside-panel";
+
+function App() {
+  return (
+    <>
+      <PanelProjects />
+    </>
+  );
+}
+
+export default App;
---
+++ managment-app/01-starting-project/src/components/Panel/aside-panel.jsx
@@ -0,0 +1,12 @@
+import "./aside-panel.css";
+
+export default function PanelProjects() {
+  return (
+    <aside className="aside-panel">
+      <div>
+        <h2 className="aside-title">Your Projects</h2>
+        <ul>list...</ul>
+      </div>
+    </aside>
+  );
+}
